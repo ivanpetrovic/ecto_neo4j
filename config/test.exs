6e03074d7c3d00c1d0a439f7511c3e79f1e0e058
Ivan Petrovic <ivan@no-wire.com>
2022-01-17 12:30:18 +0100

    Fixed deprecated Mix.Config
    replaced with import Config

--- config/test.exs
+++ config/test.exs
@@ -1,8 +1,8 @@
-use Mix.Config
+import Config
 
 config :bolt_sips, Bolt,
   hostname: 'localhost',
-  basic_auth: [username: "neo4j", password: "secret"],
+  basic_auth: [username: "", password: ""],
   port: 7687,
   pool_size: 5,
   max_overflow: 1
